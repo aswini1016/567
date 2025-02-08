952e22150c6d6aaf1d9b67ec999cada4456b0c1e
aswini1016 <aswinibumika@gmail.com>
2025-02-08 12:55:03 +0530

    training

--- src/App.js
+++ src/App.js
@@ -72,3 +72,19 @@
 // }
 
 // export default App
+
+import logo from './logo.svg'
+import './App.css';
+import MyForm from './components/myForm';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <MyForm></MyForm>
+     </div>
+  );
+}
+
+export default App;
