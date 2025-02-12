9c75d4b77cf6c5ee007c383d701abf322de247ff
aswini1016 <aswinibumika@gmail.com>
2025-02-12 19:30:15 +0530

    training

--- src/components/Sidebar1.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Slidebar1 extends Component {
-  render() {
-    return (
-      <div className='sidebar1'>Slidebar1</div>
-    )
-  }
-}
--- src/components/sidebar2.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Slidebar2 extends Component {
-  render() {
-    return (
-      <div className='sidebar2'>Slidebar2</div>
-    )
-  }
-}
