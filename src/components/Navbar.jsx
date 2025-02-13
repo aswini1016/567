92b95a1916fb4a31a9918302123c62ae9e7484c1
aswini1016 <aswinibumika@gmail.com>
2025-02-13 12:56:20 +0530

    training

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Styles from './navbar.module.css';
+import Logo from './Logo';
+import Menu from './Menu';
+
+const Navbar = () => {
+  return (
+    <section id={Styles.navBlock}>
+        <article>
+            <logo/>
+            <Menu/>
+        </article>
+    </section>
+    
+  )
+}
+
+export default Navbar
